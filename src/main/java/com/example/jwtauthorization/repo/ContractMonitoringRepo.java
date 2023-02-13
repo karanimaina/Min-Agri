34a20fd44708c9121a26ba06ff32313de3928ba7
karanimaina <karani.maina2010@gmail.com>
2023-02-13 10:52:29 +0300

    add contract repo

--- src/main/java/com/example/jwtauthorization/repo/ContractMonitoringRepo.java
+++ src/main/java/com/example/jwtauthorization/repo/ContractMonitoringRepo.java
@@ -4,5 +4,6 @@ import com.example.jwtauthorization.model.products.ContractMonitoring;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ContractMonitoringRepo  extends JpaRepository<ContractMonitoring,Long>{
+
     Long countByNameAndProductId(String name, long productId);
 }
