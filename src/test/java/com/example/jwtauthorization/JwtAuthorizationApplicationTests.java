4abbc74267f7b1588a4aee33e912b5053c83028c
karanimaina <karani.maina2010@gmail.com>
2023-02-13 08:56:49 +0300

    first commit

---
+++ src/test/java/com/example/jwtauthorization/JwtAuthorizationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jwtauthorization;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwtAuthorizationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
