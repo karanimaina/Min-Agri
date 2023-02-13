bf5dde8c7fd4de8cf0c7a04eecf9826211836921
karanimaina <karani.maina2010@gmail.com>
2023-02-13 15:55:21 +0300

    add user status

--- src/main/java/com/example/jwtauthorization/dto/UserStats.java
+++ src/main/java/com/example/jwtauthorization/dto/UserStats.java
@@ -4,5 +4,5 @@ import lombok.Data;
 
 @Data
 public class UserStats {
-    long registeredusers, activeusers, inactiveusers, newusers;
+    private long registeredUsers, activeUsers, inactiveUsers, newUsers;
 }
