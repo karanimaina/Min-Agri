eebab4e84d8b954a6c8851555bb5bc2c82abf3fc
karanimaina <karani.maina2010@gmail.com>
2023-02-13 10:16:22 +0300

    add cmmision repo

--- src/main/java/com/example/jwtauthorization/repo/CommissionRepo.java
+++ src/main/java/com/example/jwtauthorization/repo/CommissionRepo.java
@@ -1,5 +1,6 @@
 package com.example.jwtauthorization.repo;
 
+import com.example.jwtauthorization.model.products.Commission;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CommissionRepo extends JpaRepository<Commission,Long> {
