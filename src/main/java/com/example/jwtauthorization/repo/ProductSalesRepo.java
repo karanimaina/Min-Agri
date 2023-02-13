18aa9006b839fe026f89bda41347a2d5663a40df
karanimaina <karani.maina2010@gmail.com>
2023-02-13 11:26:27 +0300

    add product sales repo

--- src/main/java/com/example/jwtauthorization/repo/ProductSalesRepo.java
+++ src/main/java/com/example/jwtauthorization/repo/ProductSalesRepo.java
@@ -1,5 +1,6 @@
 package com.example.jwtauthorization.repo;
 
+import com.example.jwtauthorization.model.products.ProductSales;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ProductSalesRepo extends JpaRepository<ProductSales,Long> {
