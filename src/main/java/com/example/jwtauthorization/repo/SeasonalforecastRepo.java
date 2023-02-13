40494f063c545b4f19e7edd5a110bfd3193aceea
karanimaina <karani.maina2010@gmail.com>
2023-02-13 11:32:24 +0300

    add seasonal repo

--- src/main/java/com/example/jwtauthorization/repo/SeasonalforecastRepo.java
+++ src/main/java/com/example/jwtauthorization/repo/SeasonalforecastRepo.java
@@ -1,5 +1,6 @@
 package com.example.jwtauthorization.repo;
 
+import com.example.jwtauthorization.model.models.SeasonalForecast;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface SeasonalforecastRepo extends JpaRepository<SeasonalForecast,Long> {
---
+++ src/main/java/com/example/jwtauthorization/repo/SubsidyRepo.java
@@ -0,0 +1,11 @@
+package com.example.jwtauthorization.repo;
+
+import com.minagri.app.entities.products.Subsidy;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+public interface SubsidyRepo extends JpaRepository<Subsidy,Long> {
+    @Query("SELECT COUNT(s) FROM Subsidy s WHERE s.name=:name")
+    Long countByName(@Param("name") String name);
+}
