489c733775249ab1dd3778bf667a4fd6d1a59473
karanimaina <karani.maina2010@gmail.com>
2023-02-13 13:35:09 +0300

    add contactDto

---
+++ src/main/java/com/example/jwtauthorization/dto/ContactDto.java
@@ -0,0 +1,12 @@
+package com.example.jwtauthorization.dto;
+
+import lombok.Data;
+
+import java.util.Optional;
+
+@Data
+public class ContactDto {
+
+    String  email;
+    String   phonenumber;
+}
