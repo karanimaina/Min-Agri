2f6b223016a5bd431fafe5fc3dd387285ef90549
karanimaina <karani.maina2010@gmail.com>
2023-02-13 10:10:46 +0300

    add organisation type

---
+++ src/main/java/com/example/jwtauthorization/model/models/OrganizationTypes.java
@@ -0,0 +1,10 @@
+package com.example.jwtauthorization.model.models;
+
+import com.example.jwtauthorization.model.BaseEntity;
+
+public class OrganizationTypes extends BaseEntity {
+    String name;
+    String description;
+
+
+}
--- src/main/java/com/example/jwtauthorization/model/models/Organizations.java
+++ src/main/java/com/example/jwtauthorization/model/models/Organizations.java
@@ -1,7 +1,17 @@
 package com.example.jwtauthorization.model.models;
 
 import com.example.jwtauthorization.model.BaseEntity;
+import jakarta.persistence.Entity;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class Organizations extends BaseEntity {
     String name;
     Long orgtype;
