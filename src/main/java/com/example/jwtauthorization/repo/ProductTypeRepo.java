98bfb26195a260ef520c38f5413854114403e281
karanimaina <karani.maina2010@gmail.com>
2023-02-13 11:27:23 +0300

    add product type repo

--- src/main/java/com/example/jwtauthorization/repo/ProductTypeRepo.java
+++ src/main/java/com/example/jwtauthorization/repo/ProductTypeRepo.java
@@ -1,4 +1,11 @@
 package com.example.jwtauthorization.repo;
 
-public class ProductTypeRepo {
+import com.example.jwtauthorization.model.products.ProductType;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+public interface ProductTypeRepo extends JpaRepository<ProductType,Long> {
+    @Query("SELECT COUNT(pt) FROM ProductType pt WHERE pt.name=:name")
+    Long countByName(@Param("name") String name);
 }
