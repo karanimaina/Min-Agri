cae776ee8c9976e7e2c046442cd24fdd855ee673
karanimaina <karani.maina2010@gmail.com>
2023-02-13 10:59:16 +0300

    add dead forecast

---
+++ src/main/java/com/example/jwtauthorization/repo/DekadForecast.java
@@ -0,0 +1,6 @@
+package com.example.jwtauthorization.repo;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface DekadForecastRepo  extends JpaRepository<DekadForecast,Long> {
+}
