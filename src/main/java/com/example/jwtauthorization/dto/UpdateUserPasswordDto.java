a61a92e91aa460d677a7151db890a2ad8346ffff
karanimaina <karani.maina2010@gmail.com>
2023-02-13 14:41:49 +0300

    rename class

--- src/main/java/com/example/jwtauthorization/dto/UpdateUserPasswordDto.java
+++ src/main/java/com/example/jwtauthorization/dto/UpdateUserPasswordDto.java
@@ -3,7 +3,7 @@ package com.example.jwtauthorization.dto;
 import lombok.Data;
 
 @Data
-public class UpdateUserPassword {
+public class UpdateUserPasswordDto {
     private String phoneNumber;
     private String email;
     private String password;
